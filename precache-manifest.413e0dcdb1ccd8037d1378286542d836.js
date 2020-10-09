59067e449e47ad713f46053ab0662f173b98fa58
Keizak <keizak@yandex.ru>
2020-10-09 19:15:43 +0300

    Updates

---
+++ precache-manifest.413e0dcdb1ccd8037d1378286542d836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba72ff3b42a33a14c7d3dd39a5cfefd",
+    "url": "/LearningCardsGame/index.html"
+  },
+  {
+    "revision": "a5eb437ca92f04c30aef",
+    "url": "/LearningCardsGame/static/css/2.60eec0d8.chunk.css"
+  },
+  {
+    "revision": "81f1b0c822dc4ae024f4",
+    "url": "/LearningCardsGame/static/css/main.8008857e.chunk.css"
+  },
+  {
+    "revision": "a5eb437ca92f04c30aef",
+    "url": "/LearningCardsGame/static/js/2.70ddd9f4.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/LearningCardsGame/static/js/2.70ddd9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f1b0c822dc4ae024f4",
+    "url": "/LearningCardsGame/static/js/main.819e48d3.chunk.js"
+  },
+  {
+    "revision": "bda0b6e3b6bb73a20a4e",
+    "url": "/LearningCardsGame/static/js/runtime-main.53063317.js"
+  }
+]);
